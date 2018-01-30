d59907f889e809f2c11f0b5f3e7a458369b61178
KamilBuchwald <b.kamil9@outlook.com>
2018-01-30 22:17:27 +0100

    Complete code. Required tasks: -Indicators to prediction stocking -Method to check alternatives ways -Console Design with informations

--- src/main/prospectorsClass.java
+++ src/main/prospectorsClass.java
@@ -1,12 +1,82 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class prospectorsClass {
 
-    public static void getBidAskCrypto(){
+    public static double[] getBidAskCrypto() throws IOException{
+        String url = "https://bitbay.net/API/Public/ETHPLN/orderbook.json";
+        String USER_AGENT = "Mozilla/5.0";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+
+
+        String jObject = response.toString();
+
+
+        JSONObject json = new JSONObject(jObject);
+
+
+        JSONArray asks = (JSONArray) json.getJSONArray("asks").get(1);
+        double ask = asks.getDouble(0);
+
+
+        JSONArray bids = (JSONArray) json.getJSONArray("bids").get(1);
+        double bid = bids.getDouble(0);
+
+
+        double[] AskBid = null;
+        AskBid = new double[2];
+        AskBid[0] = ask;
+        AskBid[1] = bid;
+
+        return AskBid;
 
     }
 
-    public static void getCurrentValue(){
+    public static void getHistoricalData(){
+        // Method to get data form stock about  your last vallet value or for tests from json file.
+
+
+    }
+
+    public static double[] setCurrentValue() throws IOException {
+
+
+
+        double regularWallet = 100.00; // In future getHistoricalData() method
+        double lastBid = 3589.00;
+        double cryptoWallet = regularWallet/lastBid;
+
+        double[] regular_cryptoWallet = new double[2];
+
+        regular_cryptoWallet[0] = regularWallet;
+        regular_cryptoWallet[1] = cryptoWallet;
+
+        return regular_cryptoWallet;
 
     }
 }
--- src/main/tradeAlgorithm.java
+++ src/main/tradeAlgorithm.java
@@ -1,9 +1,66 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+
 public class tradeAlgorithm {
 
-    public static void main(String [] args){
+
+
+    public static void main(String[] args) throws IOException {
+
+      mainAlgorithm();
+      Info();
+
 
 
     }
+
+
+
+    public static boolean mainAlgorithm() throws IOException {
+
+
+            double[] Wallets = prospectorsClass.setCurrentValue(); //Wallet[0] = Regular Wallet, Wallet[1] = Crypto Wallet
+
+
+            double desireReguralDeal = Wallets[0]+(Wallets[0]*0.003);
+            double[] currentAskBid = prospectorsClass.getBidAskCrypto(); // currentAskBid[0] = ask currentAskBid[1] = bid
+            double currentStock = Wallets[1]*currentAskBid[1];
+
+            boolean decision = false;
+
+
+            if(currentStock<desireReguralDeal){
+                decision = false;
+            }else if(currentStock>desireReguralDeal){
+                decision =true;
+            }else{
+                decision = false;
+            }
+
+
+
+        return decision;
+    }
+
+    public static void Info() throws IOException {
+
+        double[] Wallets = prospectorsClass.setCurrentValue(); //Wallet[0] = Regular Wallet, Wallet[1] = Crypto Wallet
+
+
+        double desireReguralDeal = Wallets[0]+(Wallets[0]*0.003);
+        double[] currentAskBid = prospectorsClass.getBidAskCrypto(); // currentAskBid[0] = ask currentAskBid[1] = bid
+        double currentStock = Wallets[1]*currentAskBid[1];
+
+        System.out.println("Stan konta regularnego wynosi: "+Wallets[0]+" "+"Stan konta kryptowalut wynosi: "+ Wallets[1] +
+        " "+"Wymagana kwota do wykonania transakcji: "+ desireReguralDeal+"  Obecna kwota po kursie wynosi: "+currentStock );
+    }
+
+
+
+
+
 }
