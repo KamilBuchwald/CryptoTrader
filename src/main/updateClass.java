d04082ad13d2ceac4c3a3b769a86d7468c9e1831
KamilBuchwald <b.kamil9@outlook.com>
2018-02-01 20:11:54 +0000

    Commit

---
+++ src/main/updateClass.java
@@ -0,0 +1,35 @@
+package main;
+/*TO DO today 01.02.2018
+* Temporary class commit*/
+
+public class updateClass {
+
+    public static boolean Sell(boolean decision){
+
+        //Here if decision = true method will be connectiong to
+        // BitBay API and sell cryptocurrency of desired course
+        //When it will do it it will return true
+
+        return false;
+    }
+
+    public static boolean Buy(double desiredRegularDeal, double currentAsk, double currentBid){
+        //Here will be maked transaction of buy cryptocurrency of good course
+        //
+
+
+
+        return false;
+
+    }
+
+
+    public static void documentationGenerator(){
+
+        //Here will be generate JSON file with status and raport of sells or buys
+        // Should I import json librares
+
+
+
+    }
+}
